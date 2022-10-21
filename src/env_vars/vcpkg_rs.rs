2e020670bcb64a1a10772f1e16096ecfb1d086a9
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-10-21 11:37:08 -0600

    Replaced uses of VCPKGRS_NO_FOO string literal with newly-defined named static string

--- src/env_vars/vcpkg_rs.rs
+++ src/env_vars/vcpkg_rs.rs
@@ -1,3 +1,4 @@
 // should these environment variables be public?
 
 pub(crate) const VCPKGRS_TRIPLET: &'static str = "VCPKGRS_TRIPLET";
+pub(crate) const VCPKGRS_NO_FOO: &'static str = "VCPKGRS_NO_FOO";
