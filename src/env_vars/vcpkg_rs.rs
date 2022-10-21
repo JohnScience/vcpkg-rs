8bebe071bf5a3103585c71ca50d51d937ef176e5
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-10-21 13:25:55 -0600

    Replaced uses of NO_VCPKG string literal with newly-defined named static string

--- src/env_vars/vcpkg_rs.rs
+++ src/env_vars/vcpkg_rs.rs
@@ -3,6 +3,7 @@
 pub(crate) const VCPKGRS_TRIPLET: &'static str = "VCPKGRS_TRIPLET";
 pub(crate) const VCPKGRS_DISABLE: &'static str = "VCPKGRS_DISABLE";
 pub(crate) const VCPKGRS_DYNAMIC: &'static str = "VCPKGRS_DYNAMIC";
+pub(crate) const NO_VCPKG: &'static str = "NO_VCPKG";
 
 #[cfg(any(test, doctest))]
 pub(crate) const ARBITRARY_VCPKGRS_NO_FOO: &'static str = concat!("VCPKGRS_NO_", "FOO");
